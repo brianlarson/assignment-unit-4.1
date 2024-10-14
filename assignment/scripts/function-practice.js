da7a88db743f354b5af824fd65f577a37bf40e37
Brian Larson <brian@brianlarson.com>
2024-10-14 15:07:48 -0500

    Create function to return a personalized hello message with name as an arg

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,9 +15,10 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+    return `Hello, ${name}!`;
 }
 // Remember to call the function to test
+console.log('Test - should say "Hello, {name}!"', helloName('Dolly'))
 
 
 // 3. Function to add two numbers together & return the result
@@ -32,7 +33,7 @@ function multiplyThree() {
 }
 
 
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
@@ -40,19 +41,19 @@ function isPositive(number) {
   }
   return;
 }
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 
 
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast(array) {
 
 }
 
-// 7. Function to find a value in an array. Return true if the 
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find(value, array) {
 
 }
@@ -60,7 +61,7 @@ function find(value, array) {
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
 
@@ -83,8 +84,8 @@ function allPositive() {
 }
 
 
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
 
 
